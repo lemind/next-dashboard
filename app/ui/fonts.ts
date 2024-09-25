54e03fae298e0237086590c18c9a7e6648bf6c8b
Dimitry Lyomin <leminds@gmail.com>
2024-09-25 17:14:22 +0300

    Updates

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,8 @@
 import { Lusitana } from 'next/font/google';
 
-export const lusitana = Lusitana({ weight:'400' ,subsets: ['latin'] 
+export const lusitana = Lusitana({ weight:['400', '700'], subsets: ['latin'] 
 });
+
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
